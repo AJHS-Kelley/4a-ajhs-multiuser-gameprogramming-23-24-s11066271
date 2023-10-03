57f5da7080209a785241b320558d0c0552f8eb44
s11066271 <s11066271@students.duvalschools.org>
2023-10-03 14:43:00 -0400

    v0.1
    Defined somew array

--- multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 02_Collections, John Lowney, 10/3/23, v0.0
+﻿// 02_Collections, John Lowney, 10/3/23, v0.1
 
 using System;
 
@@ -8,7 +8,22 @@ namespace _02_Collections
     {
         static void Main(string[] args)
         {
-            
+            // Collections are variables that can store multiple values in one variable.
+            /* Arrays
+            -- Number of elements in an array CANNOT change.
+            -- Contents of elements in an array CAN change.
+            -- Items in the array are called ELEMENTS.
+            -- Arrays are ordered, meaning each item has a fixed position.
+            -- The position is known as the INDEX
+            -- First element is an array is index 0.
+            */
+
+            // Declaring and Defining an Array
+            string[] breakfastFoods = {"Bacon", "Waffles", "Cereal", "Parfait"};
+            int[] testScores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 099f, 4.25f};
+
+
         }
     }
 }
