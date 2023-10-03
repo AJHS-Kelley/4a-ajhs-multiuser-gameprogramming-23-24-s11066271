e3093813b9e7b38bbd45d84bcdad7eda7b6738e4
s11066271 <s11066271@students.duvalschools.org>
2023-10-03 14:32:56 -0400

    v0.0
    Added program info

--- multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 02_Collections, John Lowney, 10/3/23, v0.0
+
+using System;
+
+namespace _02_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
