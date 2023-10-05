04009b07cffecf57bbe722409d8aa148caa41ed7
s11066271 <s11066271@students.duvalschools.org>
2023-10-05 14:21:43 -0400

    v0.1c
    Did printing arrays on seperate lines

--- multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 02_Collections, John Lowney, 10/3/23, v0.1b
+﻿// 02_Collections, John Lowney, 10/3/23, v0.1c
 
 using System;
 
@@ -31,6 +31,24 @@ namespace _02_Collections
             Console.WriteLine();
             Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
             Console.WriteLine();
+
+            /* Print Array Contents -- Each Element on Seperate Line
+            Console.WriteLine("The elements for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join("\n", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join("\n", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join("\n", GPA));
+            Console.WriteLine();
+            */
+
+
+
+
+
+
+
+
 
         }
     }
