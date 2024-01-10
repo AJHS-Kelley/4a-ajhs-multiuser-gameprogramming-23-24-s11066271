bd4c8c1db07f7fad1436e44ef5ac91e67d7db679
s11066271 <s11066271@students.duvalschools.org>
2024-01-10 14:05:46 -0500

    v0.01

---
+++ multiUserGameProgramming/computer_science_exercises/06_review/programTemplate.cs
@@ -0,0 +1,43 @@
+//John Lowney, Review, v0.01
+using System;
+
+namespace UPDATEFOREACHPROGRAM
+{
+    class UPDATEFOREACHPROGRAM
+    {
+        static void Main(string[] args)
+        {
+            
+            // Declaring and Assigning variables
+            // Integar -- -2,147,483,648 to 2,147,483,647
+            // Integar = 4 Bytes
+            int number = 5;
+
+            // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            // Long = 8 Bytes 
+            long myLong = 4320980979079334;
+
+            // Float -- Stores up to 6-7 decimal digits.
+            // Float = 4 Bytes
+            float decimal = 6.4F;
+
+            // Double -- Stores up to 15 decimal digits
+            // Double = 8 Bytes
+            double myDouble = 8.340830850824D;
+
+            // Boolean -- true / false
+            // Boolean = 1 bit
+            bool isTrue = false;
+
+            // String
+            // 2 Bytes PER CHARACTER
+            // Must be in DOUBLE QUOTES " "
+            string word = "Hello";
+
+            // Character (char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2-bytes
+            char myChar = 'C';
+        }
+    }
+}
